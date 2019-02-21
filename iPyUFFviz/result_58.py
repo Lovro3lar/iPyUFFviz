e96d857837b0bf97c7383d165d203cb7ab3ef2a1
Lovro.3lar <Lovro.3lar@gmail.com>
2019-02-21 13:36:26 +0100

    added docstring and return time(boolean)

--- iPyUFFviz/result_58.py
+++ iPyUFFviz/result_58.py
@@ -3,6 +3,47 @@ import show_analysis
 import numpy as np
 
 def data58(drop, file, all_pt, dic58,places58,trans_matrix):
+    """data58
+    
+    [summary]
+    
+    Parameters
+    ----------
+    drop : string like 'Frequency Response Function'
+        Posible function data stored in dataset 58
+        possible choices:
+        'General or Unknown',
+        'Time Response',
+        'Auto Spectrum'',
+        'Cross Spectrum',
+        'Frequency Response Function',
+        'complex eigenvalue second order (velocity)'
+    file : UFF class element
+        UFF class element - file prepared for further usage
+    all_pt: (nparray(x),nparray(y), nparray(z)) list of arrays
+        list of nparray with x, array with y and an array with z coordintaes of all points
+    dic58: {'1':....,'2':....,....} python dictonary
+        dictonary with indices of dataset type 58 refer to used function type
+        at pyuff supported:
+        '0' - General or Unknown
+        '1' - Time Response
+        '2' - Auto Spectrum
+        '3' - Cross Spectrum
+        '4' - Frequency Response Function
+        '6' - Coherence
+    places58: {0: [....], 1: [....],....} dictonary
+        dictonary with informaion about whichd dataset 58 has infomaton about point in points array all_pt
+    trans_matrix: ({'n': trans. matrix,....,'index': i},...) array like
+        array of dictoraries with transformation matrix of local coordinate sistems in nodes and index of native dataset
+    
+    Returns
+    -------
+    data: numpy array of shape (3, maximal lenth of data array from all 58 datasets,len(all_pt))
+        matrix with function data from datasets 58 for all points in all three coordinates
+    time: boolean
+        is true if drop is 'Time Response' or 'General or Unknown'
+    """
+
     in_names58 ={'General or Unknown':'0',
               'Time Response':'1',
               'Auto Spectrum':'2',
@@ -10,6 +51,8 @@ def data58(drop, file, all_pt, dic58,places58,trans_matrix):
               'Frequency Response Function':'4',
               'complex eigenvalue second order (velocity)':'6'}
     drop = in_names58[drop]
+    if drop == '0' or '1':
+        time = True
     data = np.zeros((3,len(all_pt[0]),1))
     indices = dic58[drop]
     for index in indices:
@@ -57,4 +100,4 @@ def data58(drop, file, all_pt, dic58,places58,trans_matrix):
         else:
             data[:,:,:data_i.shape[2]] += data_i
     #return np.transpose(data,axes=[0,2,1])[0],np.transpose(data,axes=[0,2,1])[1],np.transpose(data,axes=[0,2,1])[2]
-    return np.transpose(data,axes=[0,2,1])
+    return np.transpose(data,axes=[0,2,1]),time
